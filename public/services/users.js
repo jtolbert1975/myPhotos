2c8967e8f9a17f4da9c9ad76bf2a661e7510feda
Jonathan Tolbert <imanjesolutions@gmail.com>
2017-05-03 08:21:21 -0400

    Fixed commit issues

---
+++ public/services/users.js
@@ -0,0 +1,18 @@
+var myPhotos = angular.module('myPhotos', []);
+//var userDataOp = angular.module('userDataOp', []);
+
+myPhotos.factory('userDataOp', function($http, $q){
+    var root = 'http://jsonplaceholder.typicode.com';
+    var deferred = $q.defer();
+    $http.get(root+ '/users').then(function(data){
+        "use strict";
+        deferred.resolve(data);
+    });
+    return{
+        getUsers: function() {
+            "use strict";
+            return deferred.promise;
+
+        }
+    };
+});
